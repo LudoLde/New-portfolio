e0c6fb107048a80a3de17485655d200a9ccb0b31
Ludovic Labaude <ludzy@MBP-de-Ludovic.lan>
2024-01-15 13:50:26 +0100

    responsive design fait reste finalisation projets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// PAGE HOME
 const loadingAppear = document.getElementById("text-principal");
 const loadingAppear2 = document.getElementById("second-text");
 
@@ -6,4 +7,34 @@ window.addEventListener("load", () => {
 });
 window.addEventListener("load", () => {
    loadingAppear2.classList.add("opacity-transition2", "opacity-100");
+});
+
+// PAGE À PROPOS
+const loadingAppear3 = document.getElementById("skills");
+
+window.addEventListener("load", () => {
+   loadingAppear3.classList.add("opacity-transition", "opacity-100");
+});
+
+// PAGE PROJETS
+document.addEventListener("DOMContentLoaded", function () {
+   const buttons = document.querySelectorAll(".show-details-btn");
+
+   buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+         const content = this.nextElementSibling;
+         content.classList.toggle("hidden");
+
+         const arrowIcon = this.querySelector(".fa-arrow-right");
+         const xmarkIcon = this.querySelector(".fa-xmark");
+
+         if (content.classList.contains("hidden")) {
+            xmarkIcon.style.display = "none";
+            arrowIcon.style.display = "inline-block";
+         } else {
+            xmarkIcon.style.display = "inline-block"; // ou 'block' selon votre préférence
+            arrowIcon.style.display = "none";
+         }
+      });
+   });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
    content: ["./*.{html,js}"],
    theme: {
       extend: {
+         screens: {
+            mobile: { min: "320px", max: "600px" },
+            tablet: { min: "601px", max: "769px" },
+            desktop: { min: "1100px", max: "1500px" },
+         },
+
          fontFamily: {
             montserrat: ["Montserrat Alternates"],
             lato: ["Lato"],
